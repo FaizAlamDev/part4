1a756e5f81f2f2e7ce6cdc43ebce2b6beafbf241
Faiz Alam <faizalam9883@protonmail.com>
2021-07-02 17:49:10 +0530

    4.4
    30 mins

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	let sum = blogs.reduce((sum, curr) => sum + curr.likes, 0)
+	return sum
+}
+
 module.exports = {
 	dummy,
+	totalLikes,
 }
