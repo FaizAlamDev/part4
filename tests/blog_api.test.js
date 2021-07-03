393cf7564df21b0fbbdabb6a24bcd63e5e6435e2
Faiz Alam <faizalam9883@protonmail.com>
2021-07-03 19:33:45 +0530

    4.11
    took 40 mins

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,28 +1,14 @@
 const mongoose = require('mongoose')
+const helper = require('./helper_blog')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-	{
-		title: 'React patterns',
-		author: 'Michael Chan',
-		url: 'https://reactpatterns.com/',
-		likes: 7,
-	},
-	{
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-	},
-]
-
 beforeEach(async () => {
 	await Blog.deleteMany({})
 
-	for (let blog of initialBlogs) {
+	for (let blog of helper.initialBlogs) {
 		let blogObject = new Blog(blog)
 		await blogObject.save()
 	}
@@ -65,8 +51,29 @@ test('POST request creates a new blog post', async () => {
 
 	const res = await api.get('/api/blogs')
 	const titles = res.body.map((t) => t.title)
-	expect(res.body).toHaveLength(initialBlogs.length + 1)
+	expect(res.body).toHaveLength(helper.initialBlogs.length + 1)
 	expect(titles).toContain('Canonical string reduction')
+})
+
+// exercise 4.11
+test('if likes are missing, default it to 0', async () => {
+	const newBlog = {
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	const blogsAtEnd = await helper.blogsInDb()
+	const blog = blogsAtEnd.find(
+		(blog) => blog.title === newBlog.title && blog.url === newBlog.url
+	)
+	expect(blog.likes).toBe(0)
 })
 
 afterAll(() => {
